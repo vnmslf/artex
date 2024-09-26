4496a95c4c44a02ecaa5b799004df1b8301cdc64
miberezkin <admin@professional161.ru>
2024-09-26 19:01:09 +0300

    master: classes -> class

--- local/php_interface/autoload.php
+++ local/php_interface/autoload.php
@@ -1,3 +1,4 @@
 <?Bitrix\Main\Loader::registerAutoLoadClasses(null, [
-	'user_class\Pict' => '/local/php_interface/user_class/classPict.php'
+	'user_class\Pict' => '/local/php_interface/user_class/classPict.php',
+    'user_class\CustomEntities' => '/local/php_interface/user_class/CustomEntities.php',
 ]);?>
